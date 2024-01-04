d826ef102e266cc059ffeba999df92778aa8e79c
dmymrin1995 <dmymrin1995@gmail.com>
2024-01-04 23:11:08 +0800

    redisign connection

--- app/config.py
+++ app/config.py
@@ -1,13 +1,20 @@
-from pydantic import BaseModel
-from dotenv import load_dotenv
-import os
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
-load_dotenv(dotenv_path=r".\app\.env")
+# load_dotenv(dotenv_path=)
 
 
-class PostgresConfig:
-    POSTGRES_USER: str = os.environ.get("POSTGRES_USER")
-    POSTGRES_PASSWORD: str = os.environ.get("POSTGRES_PASSWORD")
-    POSTGRES_HOST: str = os.environ.get("POSTGRES_HOST")
-    POSTGRES_PORT: str = os.environ.get("POSTGRES_PORT")
-    POSTGRES_DB: str = os.environ.get("POSTGRES_DB")
+class Settings(BaseSettings):
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_HOST: str
+    POSTGRES_PORT: str
+    POSTGRES_DB: str
+
+    @property
+    def DATABASE_URL_psycopg(self):
+        return f"postgresql+psycopg2://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
+
+    model_config = SettingsConfigDict(env_file=r".\app\.env")
+
+
+settings = Settings()
--- app/database/db.py
+++ app/database/db.py
@@ -1,13 +1,10 @@
-from app.config import PostgresConfig
+from app.config import settings
 from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-pc = PostgresConfig()
 
-SQLALCHEMY_DATABASE_URL = f"postgresql+psycopg2://{pc.POSTGRES_USER}:{pc.POSTGRES_PASSWORD}@{pc.POSTGRES_HOST}:{pc.POSTGRES_PORT}/{pc.POSTGRES_DB}"
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(settings.DATABASE_URL_psycopg)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 meta = MetaData(schema="bookings")
